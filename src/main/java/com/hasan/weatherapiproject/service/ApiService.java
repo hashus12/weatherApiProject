d70030b0cc9e099c7c6e0c5708c85f8cf608ca92
Hasan Hüseyin Özgen <53178769+hashus12@users.noreply.github.com>
2023-02-10 01:04:44 +0300

    Revert "çalışır vaziyette validate,expectin yok en son exchange kullanıldı."
    This reverts commit 0e2b0922faa7a165175fed880e9bf51de20303d4.

--- src/main/java/com/hasan/weatherapiproject/service/ApiService.java
+++
@@ -1,7 +0,0 @@
-package com.hasan.weatherapiproject.service;
-
-import com.hasan.weatherapiproject.entity.WeatherReport;
-
-public interface ApiService {
-    public WeatherReport getWeatherReport(String adress, String time);
-}
