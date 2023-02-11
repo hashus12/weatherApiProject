ab0c5b59b7f61071d4ab72709ca4b229907e9baf
Hasan Hüseyin Özgen <53178769+hashus12@users.noreply.github.com>
2023-02-11 03:10:29 +0300

    last version.

--- src/main/java/com/hasan/weatherapiproject/validator/ApiValidator.java
+++ src/main/java/com/hasan/weatherapiproject/validator/ApiValidator.java
@@ -8,13 +8,13 @@ public class ApiValidator {
     // the given address and time parameters are verified.
     public void validate(String adress, String time) {
         if (adress.isEmpty() && !time.isEmpty()) {
-            throw new AdressTimeNotFoundException("adress cannot be empty");
+            throw new AdressTimeNotFoundException("adress cannot be empty.");
         }
         if (time.isEmpty() && !adress.isEmpty()) {
-            throw new AdressTimeNotFoundException("time cannot be empty");
+            throw new AdressTimeNotFoundException("time cannot be empty.");
         }
         if (time.isEmpty() && adress.isEmpty()) {
-            throw new AdressTimeNotFoundException("address and time cannot be empty");
+            throw new AdressTimeNotFoundException("address and time cannot be empty.");
         }
     }
 }
